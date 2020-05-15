ef4437eba78e2468c9d0b395eeffd9496cbdd5d9
Micah <micah112358@gmail.com>
2020-05-15 13:02:46 -0500

    Create CMake project

---
+++ Hello-GiNaC/Hello-GiNaC.cpp
@@ -0,0 +1,12 @@
+﻿// Hello-GiNaC.cpp : Defines the entry point for the application.
+//
+
+#include "Hello-GiNaC.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
